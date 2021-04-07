ec103cf9c051570aee6ff9e604b14cb4413c85b1
mdpt <mada.patrik@gmail.com>
2021-04-07 14:54:15 +0200

    refactor: Use Data.Validation, generalize checks.

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
 import Data.Char
+import Data.Coerce
 import Data.Validation
 
 import Credentials
 import Error
 
+
+type Rule a = (a -> Validation Error a)
 
 minPassLength = 10
 minUsernameLength = 4
@@ -14,56 +17,68 @@ main :: IO ()
 main = do
   putStr "Enter a username.\n> "
   username <- Username <$> getLine
-  print $ validateUsername username
 
   putStr "Enter a password.\n> "
   password <- Password <$> getLine
-  print $ validatePassword password
 
+  display username password
 
-checkLength :: Int -> String -> Validation Error String
+checkLength :: Int -> Rule String
 checkLength minLength input = 
   let inputLength = length input 
   in case (inputLength < minLength) of
-    True -> Failure $ Error ["ERR: Input does not comply with minimum length restriction."]
+    True -> Failure $ Error ["Input does not comply with minimum length restriction."]
     False -> Success input
 
-checkPasswordLength :: String -> Validation Error Password
-checkPasswordLength input = Password <$> checkLength minPassLength input
-
-checkUsernameLength :: String -> Validation Error Username
-checkUsernameLength input = Username <$> checkLength minUsernameLength input
-
-requireAlphaNum :: String -> Validation Error String
+requireAlphaNum :: Rule String
 requireAlphaNum xs =
   case (all isAlphaNum xs) of
-    False -> Failure $ Error ["ERR: Input contains non-alphanumeric characters."]
+    False -> Failure $ Error ["Input contains non-alphanumeric characters."]
     True -> Success xs
 
-cleanWhiteSpace :: String -> Validation Error String
-cleanWhiteSpace "" = Failure $ Error ["ERR: Input consists solely of whitespaces."]
+cleanWhiteSpace :: Rule String
+cleanWhiteSpace "" = Failure $ Error ["Input consists solely of whitespaces."]
 cleanWhiteSpace (x : xs) = 
   case (isSpace x) of
     True -> cleanWhiteSpace xs
     False -> Success (x : xs)
 
-validatePassword :: Password -> Validation Error Password
-validatePassword (Password input) =
-  case (cleanWhiteSpace input) of
+validatePassword :: Rule Password
+validatePassword input =
+  case (coerce cleanWhiteSpace :: Rule Password) input of
     Failure err -> Failure err
-    Success _   -> 
-      requireAlphaNum input *>
-      checkPasswordLength input
+    Success _ ->
+      (coerce requireAlphaNum :: Rule Password) input *>
+      (coerce $ checkLength minPassLength :: Rule Password) input
 
-validateUsername :: Username -> Validation Error Username
-validateUsername (Username input) = 
-  case (cleanWhiteSpace input) of
+validateUsername :: Rule Username
+validateUsername input =
+  case (coerce cleanWhiteSpace :: Rule Username) input of
     Failure err -> Failure err
-    Success _   -> 
-      requireAlphaNum input *>
-      checkUsernameLength input
+    Success _ ->
+      (coerce requireAlphaNum :: Rule Username) input *>
+      (coerce $ checkLength minUsernameLength :: Rule Username) input
+
+passwordsErrors :: Password -> Validation Error Password
+passwordsErrors password =
+  case (validatePassword password) of
+    Failure err -> Failure $ (Error ["ERR Invalid password: "]) <> err
+    Success _ -> Success password
+
+usernameErrors :: Username -> Validation Error Username
+usernameErrors username =
+  case (validateUsername username) of
+    Failure err -> Failure $ (Error ["ERR Invalid username: "]) <> err
+    Success _ -> Success username
 
 makeUser :: Username -> Password -> Validation Error User
-makeUser uname pwd =
-  User <$> validateUsername uname
-       <*> validatePassword pwd
+makeUser username password =
+  User <$> usernameErrors username
+       <*> passwordsErrors password
+
+display :: Username -> Password -> IO ()
+display name password =
+  case makeUser name password of
+    Failure err -> putStr $ unlines $ coerce  err
+    Success (User uname _) ->
+      putStrLn ("Welcome, " ++ coerce uname)
